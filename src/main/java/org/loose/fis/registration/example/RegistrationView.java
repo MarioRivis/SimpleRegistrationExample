ecd448861e7b74926a33c5a3fcc24db7aa7cce97
CosMars89 <cosmars89@yahoo.com>
2020-04-10 15:59:55 +0300

    added SWING UI

---
+++ src/main/java/org/loose/fis/registration/example/RegistrationView.java
@@ -0,0 +1,83 @@
+package org.loose.fis.registration.example;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import org.loose.fis.registration.example.controllers.RegistrationController;
+import org.loose.fis.registration.example.services.UserService;
+
+public class RegistrationView extends JFrame {
+	private JButton btnRegister;
+	private JTextField txtUsername;
+	private JPasswordField txtPassword;
+	private JComboBox<String> cmbRole;
+	private RegistrationController controller;
+	
+	public RegistrationView() {
+		controller = new RegistrationController(this);
+		
+		setTitle("MyApp: Registration");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        
+        Container contentPane = this.getContentPane();
+        contentPane.setLayout(null);
+    	
+		JLabel lblUsername = new JLabel("Username:");
+        lblUsername.setBounds(10, 10, 120, 25);
+        contentPane.add(lblUsername);
+        
+        txtUsername = new JTextField();
+        txtUsername.setBounds(80, 10, 100, 25);
+        contentPane.add(txtUsername);
+        
+        JLabel lblPassword = new JLabel("Password:");
+        lblPassword.setBounds(10, 40, 120, 25);
+        contentPane.add(lblPassword);
+        
+        txtPassword = new JPasswordField();
+        txtPassword.setBounds(80, 40, 100, 25);
+        contentPane.add(txtPassword);
+        
+        JLabel lblRole = new JLabel("Role:");
+        lblRole.setBounds(10, 70, 120, 25);
+        contentPane.add(lblRole);
+        
+        String[] roles = { "Client", "Admin"};
+        cmbRole = new JComboBox<String>(roles);
+        cmbRole.setBounds(45, 70, 110, 25);
+        contentPane.add(cmbRole);
+		
+		btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+            	if (controller.checkAvailibility(txtUsername.getText(), new String(txtPassword.getPassword()), String.valueOf(cmbRole.getSelectedItem()))) {
+            		JOptionPane.showMessageDialog(null, "User successfully added", "Adding user", JOptionPane.INFORMATION_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(null, "User alreay added", "Adding user", JOptionPane.ERROR_MESSAGE);
+				}
+            }
+        });
+		btnRegister.setBounds(175, 110, 100, 40);
+		
+		contentPane.add(btnRegister);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		UserService.loadUsersFromFile();
+		
+		RegistrationView view = new RegistrationView();
+        view.setVisible(true);
+	}
+}
--- src/main/java/org/loose/fis/registration/example/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/registration/example/controllers/RegistrationController.java
@@ -1,36 +1,22 @@
 package org.loose.fis.registration.example.controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import org.loose.fis.registration.example.RegistrationView;
 import org.loose.fis.registration.example.exceptions.UsernameAlreadyExists;
 import org.loose.fis.registration.example.services.UserService;
 
 public class RegistrationController {
-
-    @FXML
-    public Text registrationMessage;
-    @FXML
-    public PasswordField passwordField;
-    @FXML
-    public TextField usernameField;
-    @FXML
-    public ChoiceBox role;
-
-    @FXML
-    public void initialize() {
-        role.getItems().addAll("Client", "Admin");
-    }
-
-    @FXML
-    public void handleRegisterAction() {
-        try {
-            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
-            registrationMessage.setText("Account created successfully!");
+	private RegistrationView view;
+	
+	public RegistrationController(RegistrationView view){
+        this.view = view;
+	}
+	
+	public boolean checkAvailibility(String username, String password, String role) {
+		try {
+            UserService.addUser(username, password, role);
+            return true;
         } catch (UsernameAlreadyExists e) {
-            registrationMessage.setText(e.getMessage());
+            return false;
         }
-    }
+	}
 }
